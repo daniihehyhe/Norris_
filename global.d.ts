cf4e328a221f5a48a7e298787adb95a50dceeab0
daniihehyhe <danidsds8@gmail.com>
2024-06-10 17:02:13 +0600

    fix_4

--- global.d.ts
+++ global.d.ts
@@ -1,13 +1,3 @@
-/// <reference types="next" />
-/// <reference types="next/types/global" />
-/// <reference types="next/image-types/global" />
-
-// Определение типов сообщений
-type EnMessages = typeof import("./messages/en.json");
-type RuMessages = typeof import("./messages/ru.json");
-type KgMessages = typeof import("./messages/kg.json");
-type KzMessages = typeof import("./messages/kz.json");
-
 // Глобальное объявление интерфейса для сообщений
 declare global {
     interface IntlMessages
