c4f380615d122b80f13d3083c52fed5b2590c1cd
daniihehyhe <danidsds8@gmail.com>
2024-06-26 15:30:16 +0600

    fix GHA1

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,25 @@
-import createNextIntlPlugin from "next-intl/plugin";
+import createNextIntlPlugin from 'next-intl/plugin';
 
-const withNextIntl = createNextIntlPlugin();
+const withNextIntl = createNextIntlPlugin({
+  // Вставьте ваши опции плагина здесь, если они есть
+});
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
+  output: 'export',
+  // другие параметры конфигурации
 };
 
 export default withNextIntl(nextConfig);
+
+
+// import createNextIntlPlugin from "next-intl/plugin";
+
+// const withNextIntl = createNextIntlPlugin();
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output:"export"
+// };
+
+// export default withNextIntl(nextConfig);
