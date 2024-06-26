5ab6310c3e771a0c2be22b29e905a24a794525e3
daniihehyhe <danidsds8@gmail.com>
2024-06-26 13:47:18 +0600

    deplow git hub actions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -31,3 +31,10 @@ module.exports = {
 		}
 	]
 };
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+	enabled: process.env.ANALYZE === 'true',
+  });
+  
+  module.exports = withBundleAnalyzer({
+	// другие конфигурации Next.js
+  });
